6c74bd3c0df8c035637be19e1bf62ec6a012e736
Amar <ampit.xd@gmail.com>
2021-02-24 04:24:15 -0500

    Delete env file

---
+++ pages/ssr.js
@@ -0,0 +1,41 @@
+import { getSession } from "next-auth/client";
+import AccessDenied from "../components/access-denied";
+
+export default function Page({ content, session }) {
+  // If no session exists, display access denied message
+  if (!session) {
+    return <AccessDenied />;
+  }
+
+  // If session exists, display content
+  return (
+    <>
+      <h1>Protected Page</h1>
+      <p>
+        <strong>{content}</strong>
+      </p>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  let content = null;
+
+  if (session) {
+    const hostname = process.env.NEXTAUTH_URL || "http://localhost:3000";
+    const options = { headers: { cookie: context.req.headers.cookie } };
+    const res = await fetch(`${hostname}/api/examples/protected`, options);
+    const json = await res.json();
+    if (json.content) {
+      content = json.content;
+    }
+  }
+
+  return {
+    props: {
+      session,
+      content,
+    },
+  };
+}
