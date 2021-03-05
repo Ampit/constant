d9d82309d2ed421c4b3c47e72ce42bfd8a414598
Amar <ampit.xd@gmail.com>
2021-03-04 21:22:21 -0500

    moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
   moduleNameMapper: {
-    "^.+\\.(css|less)$": "<rootDir>/__mock__/css_stub.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
   },
 };
