32d4c253b247bb2d0e3411392242799f1e1ad5f8
Amarpreet Singh <ampit.xd@gmail.com>
2021-03-07 23:28:10 -0500

    Docker-compose

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,17 @@ import { useEffect } from "react";
 export default function Main() {
   const [session, loading] = useSession();
   const router = useRouter();
-  // When rendering client side don't display anything until loading is complete
-  if (typeof window !== "undefined" && loading) return "Loading";
 
   useEffect(() => {
     // If no session exists, display access denied message
-    if (session) {
+    if (session && !loading) {
       // Redirect to tasks page
       router.push("/tasks");
     }
-  }, [session]);
+  }, []);
+
+  // When rendering client side don't display anything until loading is complete
+  if (typeof window !== "undefined" && loading) return "Loading";
 
   return <div className="text-center">Home Page</div>;
 }
