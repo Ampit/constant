26af1ef31897b61c535c62f950e05c72839595da
Amar <ampit.xd@gmail.com>
2021-03-17 21:13:32 -0400

    Home Page TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { useSession, getSession } from "next-auth/client";
+import { useSession, getSession, Session } from "next-auth/client";
 //import { useRouter } from "next/router";
 import { useEffect } from "react";
+import {ActionCreatorsMapObject, bindActionCreators, Dispatch} from 'redux';
 import { useQuery } from "react-query";
 import { connect } from "react-redux";
 import { FetchTasks } from "../store/actions/tasks";
@@ -8,9 +9,29 @@ import AccessDenied from "../components/access-denied";
 import { fetchTasks } from "../utils/tasks";
 import Button from "react-bootstrap/Button";
 
+type Task = {
+  id: String,
+  taskName: String,
+  complete: Boolean,
+  createdAt: String,
+};
+
+type Tasks = [Task];
+
+
+interface State {
+  tasks: Tasks
+};
+
+
+type Props = {
+  FetchTasks: (session: Session) => void;
+  tasks: Tasks
+};
+
 import { GetServerSideProps } from "next";
 
-const Dashboard = ({ FetchTasks, tasks }) => {
+const Dashboard = ({ FetchTasks, tasks } : Props) => {
   const [session, loading] = useSession();
   const totalTasks = tasks.length;
   const tasksCompleted = tasks.filter((task) => task.complete != false).length;
@@ -26,6 +47,7 @@ const Dashboard = ({ FetchTasks, tasks }) => {
     //   router.push("/tasks");
     // }
     if (isSuccess && data) {
+      // Fire up an event to update client's state with server's tasks
       FetchTasks(data);
     }
   }, [data]);
@@ -59,7 +81,7 @@ export const getServerSideProps: GetServerSideProps = async context =>  {
   };
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state: State) => {
   return { tasks: state.tasks };
 };
 
