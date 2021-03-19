85bcdf9dd19d84c4b22e557b8fafe0c0b0b19607
Ampit <ampit.xd@gmail.com>
2021-03-19 00:53:17 -0400

    SW

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,25 @@ import { wrapper } from "../store"; //Our function for store initialization //HO
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
   return (
     <QueryClientProvider client={queryClient}>
       <Provider session={pageProps.session}>
