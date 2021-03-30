9ccd79a64f9347a7dfd4e649a3f20ead3e0938d8
Ampit <ampit.xd@gmail.com>
2021-03-29 23:52:33 -0400

    Error FIx

--- utils/test-utils.tsx
+++ utils/test-utils.tsx
@@ -1,5 +1,6 @@
 // test-utils.js
 import { render } from "@testing-library/react";
+import type { AppProps /*, AppContext */ } from "next/app";
 import { Provider } from "next-auth/client";
 import Layout from "../components/Layout";
 import { wrapper } from "../store"; //Our function for store initialization //HOC wich helps us use Redux with Next.js
@@ -8,11 +9,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 // Add in any providers here if necessary:
 // (ReduxProvider, ThemeProvider, etc)
-const Providers = ({ children }: any) => {
+const Providers = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider session={pageProps.session}>
-        <Layout>{children}</Layout>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </Provider>
     </QueryClientProvider>
   );
