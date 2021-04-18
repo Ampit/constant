a10da5289c6771f741fcc73144bdde9a666ec75a
Ampit <ampit.xd@gmail.com>
2021-04-18 11:37:43 -0400

    Test

--- __tests__/AddTaskForm.test.tsx
+++ __tests__/AddTaskForm.test.tsx
@@ -1,14 +1,26 @@
-import { render, cleanup, screen } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 
+import React from "react";
+import ReactDOM from "react-dom";
 import AddTaskForm from "../components/AddTaskForm";
 
 afterEach(cleanup);
-
 describe("AddTaskForm tests", () => {
-  const addTaskMock = jest.fn();
-  test("Make sure there is an input", async () => {
-    render(<AddTaskForm AddTaskAction={addTaskMock} />);
+  test("Make sure the form component renders", async () => {
+    const div = document.createElement("div");
+    const addTaskMock = jest.fn();
+    ReactDOM.render(<AddTaskForm AddTaskAction={addTaskMock} />, div);
+  });
 
-    //expect(screen.getByPlaceholderText("Add New Task:")).toBeInTheDocument();
+  it("make sure it accepts input", () => {
+    const addTaskMock = jest.fn();
+    try {
+      const { getByLabelText } = render(
+        <AddTaskForm AddTaskAction={addTaskMock} />
+      );
+      expect(getByLabelText("Add New Task:")).toBeInTheDocument();
+    } catch (error) {
+      console.log("Error", error);
+    }
   });
 });
